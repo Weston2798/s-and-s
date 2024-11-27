35f2041ead890ddf8b53a82c68b288e56a030fda
Weston Allen <Weston.2798@gmail.com>
2024-11-27 17:17:56 -0500

    Exercise 4

--- lab1.asm
+++ lab1.asm
@@ -4,8 +4,11 @@ STDOUT equ 1
 SAFE_EXIT equ 0
 
 section .data
-    msg db "good Job!!!", 0xA  ; Message to print
-    msgLen equ $ - msg          ; Length of the message
+    msgEven db "x is even", 0xA  ; Message when x is even
+    msgEvenLen equ $ - msgEven   ; Length of the "even" message
+
+    msgOdd db "x is odd", 0xA    ; Message when x is odd
+    msgOddLen equ $ - msgOdd     ; Length of the "odd" message
 
 section .bss
     x resb 4   ; Reserve space for x
@@ -26,13 +29,31 @@ while_loop:
     cmp eax, ebx             ; Compare x with y
     jg exit_program          ; If x > y, exit the loop
 
-    ; Print the message "good Job!!!"
+    ; Check if x is even or odd
+    mov eax, [x]             ; Load x into eax
+    and eax, 1               ; Perform bitwise AND with 1 to check even/odd
+    cmp eax, 0               ; If the result is 0, x is even
+    je even_case             ; Jump to even_case if x is even
+    jne odd_case             ; Otherwise, it's odd (jump to odd_case)
+
+even_case:
+    ; Print "x is even"
     mov eax, SYS_WRITE       ; syscall number for SYS_WRITE
     mov ebx, STDOUT          ; file descriptor 1 (stdout)
-    mov ecx, msg             ; address of the message
-    mov edx, msgLen          ; length of the message
+    mov ecx, msgEven         ; address of the "even" message
+    mov edx, msgEvenLen      ; length of the "even" message
+    int 0x80                 ; invoke syscall
+    jmp increment_x          ; Jump to increment x
+
+odd_case:
+    ; Print "x is odd"
+    mov eax, SYS_WRITE       ; syscall number for SYS_WRITE
+    mov ebx, STDOUT          ; file descriptor 1 (stdout)
+    mov ecx, msgOdd          ; address of the "odd" message
+    mov edx, msgOddLen       ; length of the "odd" message
     int 0x80                 ; invoke syscall
 
+increment_x:
     ; Increment x by 1
     mov eax, [x]             ; Load x into eax
     add eax, 1               ; Increment x by 1
