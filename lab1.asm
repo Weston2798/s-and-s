d7b61a41515609ce44d1ed45445eb1833ca7269f
Weston Allen <Weston.2798@gmail.com>
2024-11-27 16:45:12 -0500

    Exercise 1

--- lab1.asm
+++ lab1.asm
@@ -1,30 +1,58 @@
 SYS_WRITE equ 4
+SYS_READ equ 3
 SYS_EXIT equ 1
 STDOUT equ 1
+STDIN equ 0
 SAFE_EXIT equ 0
 
 section .data
-    hellomsg db "Hello World From Assembly", 0xa
-    msglength equ $ - hellomsg
+    prompt db "Enter a string: ", 0xA
+    promptLen equ $ - prompt
+    maxInputLen equ 128
 
 section .bss
-    varMsg resb 4
-    varLen resb 4
+    userInput resb maxInputLen
+    inputLen resb 4
 
 section .text
     global _start
 
-    printString:
-        mov eax, SYS_WRITE
-        mov ebx, STDOUT
-        mov ecx, [varMsg]
-        mov edx, [varLen]
-        int 0x80 
+printString:
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
+    mov ecx, [varMsg]
+    mov edx, [varLen]
+    int 0x80
     ret
 
 _start:
-    
-    mov [varMsg], DWORD hellomsg
-    mov [varLen], DWORD msglength
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
+    mov ecx, prompt
+    mov edx, promptLen
+    int 0x80 
 
+    mov eax, SYS_READ
+    mov ebx, STDIN
+    mov ecx, userInput
+    mov edx, maxInputLen
+    int 0x80
+
+    mov eax, userInput
+    xor ecx, ecx
+find_length:
+    cmp byte [eax + ecx], 0
+    je done_find_length
+    inc ecx
+    jmp find_length
+done_find_length:
+    mov [inputLen], ecx
+
+    mov [varMsg], eax
+    mov [varLen], ecx
     call printString
+
+    ; Exit the program
+    mov eax, SYS_EXIT
+    xor ebx, ebx
+    int 0x80
