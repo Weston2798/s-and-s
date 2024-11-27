2610f2947e1275c1056cc78a1659bea339a0bea8
Weston Allen <Weston.2798@gmail.com>
2024-11-27 16:57:52 -0500

    Exercise 2

--- lab1.asm
+++ lab1.asm
@@ -1,60 +1,60 @@
 SYS_WRITE equ 4
-SYS_READ equ 3
 SYS_EXIT equ 1
 STDOUT equ 1
 STDIN equ 0
-SAFE_EXIT equ 0
 
 section .data
-    prompt db "Enter a string: ", 0xA
-    promptLen equ $ - prompt
-    maxInputLen equ 128
+    msgGreater db "valA is greater than valB", 0xA  ; Message when valA > valB
+    msgGreaterLen equ $ - msgGreater                ; Length of the message
 
-section .bss
-    userInput resb maxInputLen
-    inputLen resb 4
-    varMsg resb 4
-    varLen resb 4 
+    msgLess db "valA is less than valB", 0xA       ; Message when valA < valB
+    msgLessLen equ $ - msgLess                      ; Length of the message
+
+    msgEqual db "valA is equal to valB", 0xA       ; Message when valA == valB
+    msgEqualLen equ $ - msgEqual                    ; Length of the message
 
 section .text
     global _start
 
-printString:
-    mov eax, SYS_WRITE
-    mov ebx, STDOUT
-    mov ecx, [varMsg]
-    mov edx, [varLen]
-    int 0x80
-    ret
-
 _start:
+    ; Initialize valA and valB
+    mov eax, 10        ; valA = 10
+    mov ebx, 15        ; valB = 15
+
+    ; Compare valA and valB
+    cmp eax, ebx       ; Compare valA with valB
+    je equal_case      ; If valA == valB, jump to equal_case
+    ja greater_case    ; If valA > valB, jump to greater_case
+    jb less_case       ; If valA < valB, jump to less_case
+
+equal_case:
+    ; Print "valA is equal to valB"
     mov eax, SYS_WRITE
     mov ebx, STDOUT
-    mov ecx, prompt
-    mov edx, promptLen
-    int 0x80 
+    mov ecx, msgEqual
+    mov edx, msgEqualLen
+    int 0x80
+    jmp exit_program
 
-    mov eax, SYS_READ
-    mov ebx, STDIN
-    mov ecx, userInput
-    mov edx, maxInputLen
+greater_case:
+    ; Print "valA is greater than valB"
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
+    mov ecx, msgGreater
+    mov edx, msgGreaterLen
+    int 0x80
+    jmp exit_program
+
+less_case:
+    ; Print "valA is less than valB"
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
+    mov ecx, msgLess
+    mov edx, msgLessLen
     int 0x80
 
-    mov eax, userInput
-    xor ecx, ecx
-find_length:
-    cmp byte [eax + ecx], 0
-    je done_find_length
-    inc ecx
-    jmp find_length
-done_find_length:
-    mov [inputLen], ecx
-
-    mov [varMsg], eax
-    mov [varLen], ecx
-    call printString
-
+exit_program:
     ; Exit the program
     mov eax, SYS_EXIT
-    xor ebx, ebx
-    int 0x80
+    xor ebx, ebx       ; Return code 0
+    int 0x80           ; Invoke the system call to exit
